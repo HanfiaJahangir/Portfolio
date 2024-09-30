4cfa56052ec3c5b1e5a0ed2488bab748e3e6ad8f
BHGS <Blackholegamesstudio@gmail.com>
2024-09-30 21:48:05 +0500

    avatar , about home js updated

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/avatar.png";
 import Tilt from "react-parallax-tilt";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
+import { AiFillGithub, AiFillMail } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
@@ -19,37 +15,30 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I am a passionate game developer with extensive experience in building immersive gaming experiences, particularly in Unity3D. My journey started with a love for programming, and I have worked on multiple complex and innovative game projects.
               <br />
-              <br />I am fluent in classics like
+              <br />
+              I am highly skilled in 
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
-              </i>
+                <b className="purple"> C++, C#, and TypeScript. </b>
+              </i> 
+              These skills have enabled me to create dynamic and interactive multiplayer games.
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              My expertise lies in developing new and engaging &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
+                <b className="purple">game mechanics, multiplayer features,</b>
+              </i> and integrating APIs for enhanced gameplay.
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              Some of my key projects include:
               <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
+                <b className="purple"> Pollen Pop, Kart Racing League, Crystale, </b>
               </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              and more.
+              <br />
+              <br />
+              I enjoy collaborating with design and art teams to ensure a seamless user experience (UX) and improve gameplay using analytics like Unity Analytics and OneSignal.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
@@ -67,7 +56,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/HanfiaJahangir"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -77,17 +66,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/hanfia-jahangir-793861109/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,12 +76,12 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="mailto:hanfia.jahangir@gmail.com"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
                 >
-                  <AiFillInstagram />
+                  <AiFillMail />
                 </a>
               </li>
             </ul>
